6d7324c6dd673219642bd6b977169dddd9744ed2
Aman Nasir <aman.nasir231@gmail.com>
2023-02-02 16:55:31 +0500

    bcrypt added

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -9,7 +9,13 @@ const loginUser = async (req, res) => {
 // Signup User
 
 const SignupUser = async (req, res) => {
-  res.json({ message: "Signup User" });
+  const { email, password } = req.body;
+  try {
+    const user = await User.signup(email, password);
+    res.status(200).json({ email, user });
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
 };
 
 module.exports = { loginUser, SignupUser };
