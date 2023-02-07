2bc6809a44a3c9f5a67a1724e585ebad8489797a
Aman Nasir <aman.nasir231@gmail.com>
2023-02-08 01:17:37 +0500

    Auth Added

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -24,10 +24,6 @@ userSchema.statics.signup = async function (email, password) {
     throw Error("Not a valid email");
   }
 
-  if (!validator.isStrongPassword(password)) {
-    throw Error("Not a valid Password");
-  }
-
   const exists = await this.findOne({ email });
   if (exists) {
     throw Error("Email Already in use");
@@ -36,6 +32,21 @@ userSchema.statics.signup = async function (email, password) {
   const salt = await bcrypt.genSalt(10);
   const hash = await bcrypt.hash(password, salt);
   const user = await this.create({ email, password: hash });
+  return user;
+};
+
+userSchema.statics.login = async function (email, password) {
+  if (!email || !password) {
+    throw Error("All Field must be filled ");
+  }
+  const user = await this.findOne({ email });
+  if (!user) {
+    throw Error("Incorrect Email");
+  }
+  const match = await bcrypt.compare(password, user.password);
+  if (!match) {
+    throw Error("Incorrect Password");
+  }
   return user;
 };
 
